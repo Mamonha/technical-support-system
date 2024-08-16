85a3fbed5c211ab681885584cfc51adb32feb335
Mamonha <mamonha302@gmail.com>
2024-08-15 22:25:29 -0300

    Creating store/index methods in service user

--- src/main/java/app/services/UserService.java
+++ src/main/java/app/services/UserService.java
@@ -1,5 +1,25 @@
 package app.services;
 
-public class UserService {
+import app.entities.User;
+import app.resositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    protected UserRepository userRepository;
+    public void store(User user) throws Exception {
+        try {
+            userRepository.save(user);
+        } catch (Exception e) {
+            throw new Exception("Failed to create user: " + e.getMessage());
+        }
+    }
+
+    public List<User> index(){
+        return userRepository.findAll();
+    }
 }
