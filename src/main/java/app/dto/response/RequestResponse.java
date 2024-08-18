664e39bd87e213412beeeefc39fad69aeaead5ef
Mamonha <mamonha302@gmail.com>
2024-08-18 15:51:00 -0300

    Adjustment in request response dto

--- src/main/java/app/dto/response/RequestResponse.java
+++ src/main/java/app/dto/response/RequestResponse.java
@@ -1,36 +1,30 @@
 package app.dto.response;
 
 import app.entities.Response;
-import app.entities.Ticket;
-import app.entities.User;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
 public class RequestResponse {
 
     @NotBlank(message = "Description is mandatory")
-    @Max(value = 700, message ="Maximum characters 700" )
+    @Size(max = 700, message ="Maximum characters 700" )
     private String description;
 
-    @PastOrPresent
-    private LocalDateTime dateTime;
+    @NotNull
+    private Long userId;
 
     @NotNull
-    private User user;
-
-    @NotNull
-    private Ticket ticket;
+    private Long ticketId;
 
     public Response response(){
         Response response= new Response();
         response.setDescription(this.description);
-        response.setDateTime(this.dateTime);
-        response.setUser(this.user);
-        response.setTicket(this.ticket);
         return response;
     }
 }
