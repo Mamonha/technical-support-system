782bcb98569acdf15500c9bed484beb4e97cb6b7
Mamonha <mamonha302@gmail.com>
2024-08-18 15:52:46 -0300

    Fix: changing resositories to repositories

---
+++ src/main/java/app/repositories/TicketRepository.java
@@ -0,0 +1,8 @@
+package app.repositories;
+
+import app.entities.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketRepository extends JpaRepository <Ticket, Long> {
+
+}
