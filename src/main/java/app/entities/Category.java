94b2ddebf58558e0218b40d3bbcf5c86339d289a
Mamonha <mamonha302@gmail.com>
2024-08-18 15:51:55 -0300

    Adding json reference

--- src/main/java/app/entities/Category.java
+++ src/main/java/app/entities/Category.java
@@ -1,5 +1,6 @@
 package app.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,5 +26,6 @@ public class Category {
     private String description;
 
     @ManyToMany (mappedBy = "listCategory")
+    @JsonBackReference
     private List<Ticket> list;
 }
