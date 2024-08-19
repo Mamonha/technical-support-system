27b85293510e339d1327e6b23fa6e59eccb80652
Mamonha <mamonha302@gmail.com>
2024-08-19 19:42:41 -0300

    Adjustment priority/status in response ticket

--- src/main/java/app/dto/Ticket/ResponseTicket.java
+++ src/main/java/app/dto/Ticket/ResponseTicket.java
@@ -3,6 +3,8 @@ package app.dto.Ticket;
 import app.dto.category.ResponseCategory;
 import app.dto.user.ResponseUser;
 import app.entities.Ticket;
+import app.enums.Priority;
+import app.enums.TicketStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,9 +21,9 @@ public class ResponseTicket {
     private Long id;
     private String title;
     private String description;
-    private int priority;
+    private Priority priority;
     private LocalDateTime dateTime;
-    private int status;
+    private TicketStatus status;
     private ResponseUser user;
     private List<ResponseCategory> categories;
 
@@ -34,9 +36,9 @@ public class ResponseTicket {
             ticket.getId(),
             ticket.getTitle(),
             ticket.getDescription(),
-            ticket.getPriority(),
+            Priority.fromValue(ticket.getPriority()),
             ticket.getDateTime(),
-            ticket.getStatus(),
+            TicketStatus.fromValue(ticket.getStatus()),
             ResponseUser.user(ticket.getUser()),
             categoryDTOs
         );
